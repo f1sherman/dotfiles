ec903224adfea374541ca6a0017f934f437741ad
Brian John <brian@brianjohn.com>
2014-03-25 15:49:54 -0500

    show battery percentage in menu bar

--- osx/set_defaults.sh
+++ osx/set_defaults.sh
@@ -101,3 +101,7 @@ sudo nvram boot-args="-v"
 
 # Disable mouse acceleration
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+# Show battery percentage in menu bar
+defaults write com.apple.menuextra.battery ShowPercent -string "NO"
+defaults write com.apple.menuextra.battery ShowTime -string "YES"
